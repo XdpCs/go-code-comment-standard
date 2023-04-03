7dcecf3381596631425ac8afa95da744241f53f2
XdpCs <451679250@qq.com>
2023-04-03 13:22:25 +0800

    refactor(all): update all comment
    update all comment

--- demo/main.go
+++ demo/main.go
@@ -1,13 +1,12 @@
 // main is a user's login Service.
-
 package main
 
 import "fmt"
 
-// @Title  main.go
-// @Description main.go contains the user login function and logout function
-// @Author  XdpCs 2023/3/13 11:14
-// @Update  XdpCs 2023/3/13 11:18
+// @Title        main.go
+// @Description  main.go contains the user login function and logout function
+// @Create       XdpCs 2023-03-13 11:14
+// @Update       XdpCs 2023-04-03 11:20
 
 // User defines user login info
 type User struct {
@@ -24,11 +23,10 @@ type IUser interface {
 var _ IUser = (*User)(nil)
 
 // Login
-// @Title       Login
-// @Description User login into the system and print success or print failed
-// @Author      XdpCs 2023/3/13 11:14
-// @Param     	nil
-// @Return    	nil
+// @Description    User login into the system and print success or print failed
+// @Create         XdpCs 2023-04-03 13:12:23
+// @Modified           XdpCs 2023-04-03 13:18:00
+// @Modify description Fix comment
 func (user *User) Login() {
 	if user.UserName == "XdpCs" && user.Password == "11141118" {
 		fmt.Println("登录成功")
@@ -38,22 +36,18 @@ func (user *User) Login() {
 }
 
 // Logout
-// @Title       Logout
-// @Description User logout the system and print success
-// @Author      XdpCs 2023/3/13 11:14
-// @Param     	nil
-// @Return    	nil
+// @Description    User logout the system and print success
+// @Create         XdpCs 2023/3/13 11:14
 func (user *User) Logout() {
 	fmt.Println("注销成功")
 }
 
 // Register
-// @Title       Register
-// @Description User login into the system and print success or print failed
-// @Author      XdpCs 2023/3/13 11:14
-// @Param     	userName string "user's name"
-// @Param 		password string "user's password"
-// @Return      anonymity *User "User object contains userName and password"
+// @Description    User login into the system and print success or print failed
+// @Create         XdpCs 2023-04-03 13:20:40
+// @Param          userName string user's name
+// @Param          password string user's password
+// @Return         *User object contains userName and password
 func Register(userName string, password string) *User {
 	return &User{
 		UserName: userName,
